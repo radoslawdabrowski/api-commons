e939424bdacfa23a4559166017c9ec816fef52ab
Radosław Dąbrowski <kontakt@radoslawdabrowski.pl>
2020-07-22 11:50:06 +0200

    init

---
+++ src/main/java/online/buildit/commons/httpapicommon/HttpApiCommonApplication.java
@@ -0,0 +1,13 @@
+package online.buildit.commons.httpapicommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpApiCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpApiCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/buildit/commons/httpapicommon/HttpApiCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package online.buildit.commons.httpapicommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpApiCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
