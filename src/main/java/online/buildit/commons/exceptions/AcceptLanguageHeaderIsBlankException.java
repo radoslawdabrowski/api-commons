4671bbedd4b51be9fed074d1714adb7c5b480a23
Radosław Dąbrowski <kontakt@radoslawdabrowski.pl>
2020-11-04 21:16:11 +0100

    refactor code for locale resolver

---
+++ src/main/java/online/buildit/commons/exceptions/AcceptLanguageHeaderIsBlankException.java
@@ -0,0 +1,5 @@
+package online.buildit.commons.exceptions;
+
+public class AcceptLanguageHeaderIsBlankException extends Exception{
+
+}
--- src/main/java/online/buildit/commons/resolvers/CommonLocaleResolver.java
+++ src/main/java/online/buildit/commons/resolvers/CommonLocaleResolver.java
@@ -1,9 +1,12 @@
 package online.buildit.commons.resolvers;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import online.buildit.commons.enums.Characters;
+import online.buildit.commons.exceptions.AcceptLanguageHeaderIsBlankException;
 import org.apache.commons.lang3.LocaleUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -16,23 +19,31 @@ import java.util.Locale;
 @Component
 public class CommonLocaleResolver extends SessionLocaleResolver {
 
+    @Value("headers.acceptLanguageHeader")
+    public String ACCEPT_LANGUAGE_HEADER;
+
     @Override
     public Locale resolveLocale(final HttpServletRequest httpServletRequest) {
-        final String language = httpServletRequest.getHeader("Accept-Language");
+        final String language = getLanguageHeader(httpServletRequest);
 
-        if (!StringUtils.isBlank(language)) {
-            try {
-                if (language.indexOf(Characters.COMMA) > 0) {
-                    return LocaleUtils.toLocale(language.substring(0, language.indexOf(Characters.COMMA)));
-                } else {
-                    return LocaleUtils.toLocale(language.replace(Characters.HYPHEN, Characters.UNDERSCORE));
-                }
-            } catch (final IllegalArgumentException ex) {
-                return Locale.getDefault();
-            }
+        try {
+            return LocaleUtils.toLocale(this.getLocaleName(language));
+        } catch (final IllegalArgumentException | AcceptLanguageHeaderIsBlankException ex) {
+            return Locale.getDefault();
         }
+    }
 
-        return Locale.getDefault();
+    private String getLocaleName(@NonNull final String language) throws AcceptLanguageHeaderIsBlankException {
+        if (StringUtils.isBlank(language)) {
+            throw new AcceptLanguageHeaderIsBlankException();
+        }
+        return language.indexOf(Characters.COMMA) > 0?
+            language.substring(0, language.indexOf(Characters.COMMA)):
+            language.replace(Characters.HYPHEN, Characters.UNDERSCORE);
+    }
+
+    private String getLanguageHeader(final HttpServletRequest request) {
+        return request.getHeader(ACCEPT_LANGUAGE_HEADER);
     }
 
     @Override
